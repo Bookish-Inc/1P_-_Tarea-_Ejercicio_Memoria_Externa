479d864316f8068b5b63836b0cfe4679bd9e9d64
hlbv17 <helenlbv17@gmail.com>
2022-12-26 14:33:45 -0500

    acerca de

--- app/src/main/java/com/example/inicio8_6/AcercaDe.java
+++ app/src/main/java/com/example/inicio8_6/AcercaDe.java
@@ -2,7 +2,10 @@ package com.example.inicio8_6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AcercaDe extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class AcercaDe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+
+        Button btnRegreso = (Button) findViewById(R.id.btn_regresar);
+
+        btnRegreso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Llamar a Login
+                Intent call = new Intent(view.getContext(), Login.class);
+                startActivity(call);
+            }
+        });
     }
 }
